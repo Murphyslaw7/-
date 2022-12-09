8416a885e2bf9a9a92bf3c36a466bdb6121b279f
Murphyslaw7 <murphytremar2@gmail.com>
2022-12-09 12:59:02 -0500

    Main

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,4 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'UN';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : ""
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
-
-  return {
-    name,
-    blogTitle,
-    footerText,
-  };
-};
+    : 'UN';}
