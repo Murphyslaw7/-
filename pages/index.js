2f3a7a4c61f4d1e22c75a6b13e24e8fa3ed516a3
Murphyslaw7 <murphytremar2@gmail.com>
2022-12-09 16:02:21 -0500

    main

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Index({ posts, globalData }) {
                 as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
                 href={`/posts/[slug]`}
               >
-                <a className="">
+                <a className="py-6 lg:py-10 px-6 lg:-16 block focus:outline-none focusing:ring-4">
                   {post.data.date && (
                     <p className="uppercase mb-3 font-bold opacity-60">
                       {post.data.date}
